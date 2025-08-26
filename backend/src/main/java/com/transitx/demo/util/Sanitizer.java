477354f0dec3fd8e67179d9b9f20700fc5b5267d
Ritu786 <65646433+Ritu786@users.noreply.github.com>
2025-08-26 12:26:29 +0400

    Add Sanitizer class for string escaping

---
+++ backend/src/main/java/com/transitx/demo/util/Sanitizer.java
@@ -0,0 +1,9 @@
+package com.transitx.demo.util;
+
+public class Sanitizer {
+  public static String escape(String s) {
+    if (s == null) return "";
+    return s.replace("&","&amp;").replace("<","&lt;").replace(">","&gt;")
+            .replace("\"","&quot;").replace("'","&#x27;");
+  }
+}
